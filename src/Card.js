863306835426d73265b310e9f12ed49304c104d4
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-15 15:09:31 +0000

    card uses new properties

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,14 @@
 import React from 'react'
 
-export default function Card({key, image}) {
-    const angle = Math.random() * 40 - 20;   // between -20 and 20
-    const randomX = Math.random() * 20 - 10; // between -10 and 10
-    const randomY = Math.random() * 20 - 10; // between -10 and 10
-    const zIndex = key;
+export default function Card({card}) {
     const style = {
-        transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`,
-        zIndex: zIndex,
+        transform: `translate(${card.X}px, ${card.Y}px) rotate(${card.angle}deg)`,
+        zIndex: card.zIndex,
         position: 'absolute',
         top: '20%',
         left: '45%'
     }
   return (
-    <img src={image} alt={code} style={style} />
+    <img src={card.image} style={style} />
   )
 }
