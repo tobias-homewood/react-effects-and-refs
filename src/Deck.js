f64b1c53c1e497828f4a0a2464f7c82660199ea0
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-15 15:14:20 +0000

    code format + button is removed after drawing 52 cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
-export default function Deck({cards, drawCard, shuffleDeck, shuffling, drawing}) {
-  return (
-    <>
-    <button onClick={shuffleDeck} disabled={shuffling || drawing}>{shuffling ? "Shuffling..." : "Shuffle Deck"}</button>
-    <button onClick={drawCard} disabled={shuffling || drawing}>{drawing ? "Drawing a Card..." : "Draw a Card"}</button>
-      {cards.map((card, index) => 
-        <Card key={index} card={card} />
-      )}
-    </>
-  )
+export default function Deck({
+    cards,
+    drawCard,
+    shuffleDeck,
+    shuffling,
+    drawing,
+}) {
+    return (
+        <>
+            <button onClick={shuffleDeck} disabled={shuffling || drawing}>
+                {shuffling ? "Shuffling..." : "Shuffle Deck"}
+            </button>
+            {cards.length < 52 && (
+                <button onClick={drawCard} disabled={shuffling || drawing}>
+                    {drawing ? "Drawing a Card..." : "Draw a Card"}
+                </button>
+            )}
+            {cards.map((card, index) => (
+                <Card key={index} card={card} />
+            ))}
+        </>
+    );
 }
