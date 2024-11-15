6ea35d0eff3f2c84fbcc2fe8b12151859b0e7bb9
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-15 15:09:54 +0000

    deck functions and state handling

--- src/Deck.js
+++ src/Deck.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Card from './Card'
 
-export default function Deck({cards}) {
+export default function Deck({cards, drawCard, shuffleDeck, shuffling, drawing}) {
   return (
     <>
-    <button onClick={drawCard}>Get Card</button>
+    <button onClick={shuffleDeck} disabled={shuffling || drawing}>{shuffling ? "Shuffling..." : "Shuffle Deck"}</button>
+    <button onClick={drawCard} disabled={shuffling || drawing}>{drawing ? "Drawing a Card..." : "Draw a Card"}</button>
       {cards.map((card, index) => 
-        <Card key={index} image={card.image} />
+        <Card key={index} card={card} />
       )}
     </>
   )
